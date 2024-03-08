3d459ed8af8d782a163961f6540ce024644ed2fe
Andrew Duffy <andreweduffy@gmail.com>
2024-03-07 21:02:16 -0500

    this?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,15 +9,24 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_stream(stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+static PONG: &'static str = "+PONG\r\n";
+
+/// process a single stream, read a command and send back the value.
+fn handle_stream(mut stream: TcpStream) -> anyhow::Result<()> {
+    stream.write(PONG.as_bytes())?;
+
+    Ok(())
 }
